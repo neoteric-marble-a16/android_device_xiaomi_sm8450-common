d1ce074e147e5a60b1f9c20173fb7c36cb2b124b
ShadeDark6 <nizamfa6@gmail.com>
2025-11-16 15:33:55 +0000

    marble: Patch audio blobs to use libaudioroute-v34

--- extract-files.sh
+++ extract-files.sh
@@ -100,7 +100,10 @@ function blob_fixup() {
             ;;
         vendor/lib64/hw/com.qti.chi.override.so | vendor/lib64/libcamxcommonutils.so | vendor/lib64/libmialgoengine.so)
             "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "$2"
-            ;;            
+            ;;
+        vendor/lib64/libagm.so | vendor/lib64/libar-pal.so | vendor/lib64/libaudioroute_ext.so | vendor/lib64/libfmpal.so | vendor/lib64/libkaraokepal.so | vendor/lib64/libmcs.so | vendor/lib64/libmmhardware.so)
+            "${PATCHELF}" --replace-needed "libaudioroute.so" "libaudioroute-v34.so" "${2}"
+            ;;                        
     esac
 }
 
