51c0a317468f6eeb51dd20fb139e0bb9a19bff62
ShadeDark6 <nizamfa6@gmail.com>
2025-12-13 22:46:29 +0000

    parts: Add per-app refresh rate
    From : https://github.com/peridot-hyperos-2/device_xiaomi_peridot/commits/aosp-16/parts/src/org/lineageos/settings/refreshrate

--- parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.kt
+++ parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.kt
@@ -16,6 +16,7 @@ import co.aospa.xiaomiparts.display.DcDimmingService
 import co.aospa.xiaomiparts.doze.PocketService
 import co.aospa.xiaomiparts.gestures.GestureUtils
 import co.aospa.xiaomiparts.thermal.ThermalUtils
+import co.aospa.xiaomiparts.refreshrate.RefreshUtils
 import co.aospa.xiaomiparts.touch.HighTouchPollingService
 import co.aospa.xiaomiparts.touch.TouchOrientationService
 
@@ -34,6 +35,7 @@ class BootCompletedReceiver : BroadcastReceiver() {
         TouchOrientationService.startService(context)
         HighTouchPollingService.startService(context)
         ThermalUtils.getInstance(context).startService()
+        RefreshUtils.startService(context)       
         GestureUtils.onBootCompleted(context)
     }
 
